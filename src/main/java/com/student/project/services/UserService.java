852cf44280d71c97cac85566384c2de49507e7db
Gildesio-af <gildesioaf@alu.ufc.br>
2024-02-16 22:08:13 -0300

    Service layer, component registration

---
+++ src/main/java/com/student/project/services/UserService.java
@@ -0,0 +1,26 @@
+package com.student.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.student.project.entities.User;
+import com.student.project.repositories.UserRespository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRespository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
